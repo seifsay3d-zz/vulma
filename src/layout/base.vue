fd5b4c5bb8d82168216979550b745a19c0bfeea5
seifsayed <seif.sayed@gmail.com>
2017-11-02 01:12:06 +0200

    add footer

--- src/layout/base.vue
+++ src/layout/base.vue
@@ -2,14 +2,17 @@
   <div>
     <v-header></v-header>
     <router-view></router-view>
+    <v-footer></v-footer>
   </div>
 </template>
 <script>
   import VHeader from '@/layout/header';
+  import VFooter from '@/layout/footer';
 
   export default{
     components: {
       VHeader,
+      VFooter,
     },
   };
 </script>
---
+++ src/layout/footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        <p>
+          <strong>Vulma</strong> by <a href="http://seifsayed.com">Seif Sayed</a>. The source code is licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+        </p>
+      </div>
+    </div>
+  </footer>
+</template>
