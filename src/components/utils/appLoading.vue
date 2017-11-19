4d81384f39e31b8f7c51aa9385293b16782ae2f1
seifsay3d <seif.sayed@gmail.com>
2017-11-19 18:52:52 +0200

    add loading components

---
+++ src/components/utils/appLoading.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="wrapper">
+      <div class="loading style-1"></div>  
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import '../../assets/scss/vars';
+.wrapper{
+  text-align:center;
+  width:100%;
+  height:100%;
+}
+.loading {
+  position: relative;
+  top:48vh;
+  display: inline-block;
+  border-width: 30px;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+}
+
+.style-1 {
+  border-style: solid;
+  border-color: $primary transparent;
+}
+
+@-webkit-keyframes spin {
+  100% { -webkit-transform: rotate(359deg); }
+}
+
+@-moz-keyframes spin {
+  100% { -moz-transform: rotate(359deg); }
+}
+
+@-o-keyframes spin {
+  100% { -moz-transform: rotate(359deg); }
+}
+
+@keyframes spin {
+  100% {  transform: rotate(359deg); }
+}
+</style>
---
+++ src/components/utils/loading.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="wrapper has-text-centered" v-if="pending || error">
+      <div v-if="pending">
+        <div class="loading style-1"></div>
+      </div>
+      <div v-if="error">
+        smthing went wrong
+      </div>  
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    props:{
+      error: {
+        default: false,
+      },
+      pending:{
+        default: true,
+      },
+    },
+  };
+</script>
+<style lang="scss" scoped>
+@import '../../assets/scss/vars';
+  .wrapper{
+    margin:0 auto;
+    width:250px;
+  }  
+  .loading {
+  display: inline-block;
+  border-width: 10px;
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  }
+
+.style-1 {
+  border-style: solid;
+  border-color: $primary transparent;
+  }
+
+@-webkit-keyframes spin {
+  100% { -webkit-transform: rotate(359deg); }
+  }
+
+@-moz-keyframes spin {
+  100% { -moz-transform: rotate(359deg); }
+  }
+
+@-o-keyframes spin {
+  100% { -moz-transform: rotate(359deg); }
+  }
+
+@keyframes spin {
+  100% {  transform: rotate(359deg); }
+  }
+</style>
