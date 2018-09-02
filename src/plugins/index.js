eed4989d0a3fa6843914165f88d10e35f279e17f
Hosseiny <hosseiny>
2018-09-02 16:29:03 +0200

    install new packages for Notifications, Authentication, Media Queries, Axios & more

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -14,12 +14,37 @@ import VueI18n from 'vue-i18n';
 import VueScrollTo from 'vue-scrollto';
 import createPersist from 'vuex-localstorage';
 import router from '../router';
+import vMediaQuery from 'v-media-query';
+import checkView from 'vue-check-view'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import VTooltip from 'v-tooltip'
+import Buefy from 'buefy'
+import vueSlider from 'vue-slider-component';
+import VeeValidate from 'vee-validate';
+import Notifications from 'vue-notification'
+import { VueAuthenticate } from 'vue-authenticate';
+import ReadMore from 'vue-read-more';
 
+Vue.use(ReadMore);
+Vue.use(Notifications)
+Vue.use(VeeValidate);
+Vue.component('vue-slider',vueSlider);
+Vue.use(Buefy);
+Vue.use(VueAxios, axios);
+Vue.use(checkView)
+Vue.use(VTooltip);
 Vue.use(SocialSharing);
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyBvWE_sIwKbWkiuJQOf8gSk9qzpO96fhfY',
     libraries: 'places',
+  },
+});
+Vue.use(vMediaQuery, {
+  variables: {
+    tablet: '770px',
+    miniLaptop: '990px',
   },
 });
 Vue.use(VueAnalytics, {
@@ -35,9 +60,9 @@ Vue.use(VueI18n);
 Vue.use(FlagIcon);
 Vue.use(VueScrollTo, {
      container: "body",
-     duration: 500,
+     duration: 800,
      easing: "ease",
-     offset: 0,
+     offset: -70,
      cancelable: true,
      onDone: false,
      onCancel: false,
